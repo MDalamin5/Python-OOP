12f4380569480204bc9c3584b9ed6cf2e3441439
MDalamin5 <mdal.amin5@northsouth.edu>
2023-06-17 13:12:06 +0600

    Module 1 done

---
+++ Week 1/Module 01/Lecture/for.py
@@ -0,0 +1,21 @@
+# for(int i=0;i<10;i++)
+# {
+
+# }
+numbers = [5,10,33,23,21]
+sum=0
+for num in numbers:
+    sum+=num
+    # print(num)
+
+print(sum)  
+
+name = "Al Amin"
+for char in name:
+    print(char)
+
+# range
+for i in range(1,10,5):
+    print(i)    
+
+
