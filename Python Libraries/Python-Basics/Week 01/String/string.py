830507d98e8df0bb8e8f5a9597c73b84517c8782
MDalamin5 <mdal.amin5@northsouth.edu>
2023-09-12 19:11:00 +0600

    python-string

---
+++ Python Libraries/Python-Basics/Week 01/String/string.py
@@ -0,0 +1,18 @@
+first = 'Md'
+last = 'Al Amin'
+name = first + last
+# print(name)
+print(f'{first} {last}')
+print(name[0:5])
+print(name[-1])
+print(len(name))
+my_food = 'Polaw beef rost and cock'
+print('rost' in my_food)
+print('Polaw' not in my_food)
+lorem_text = """luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sit amet aliquam id diam maecenas ultricies mi eget mauris pharetra et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam"""
+print(lorem_text)
+
+my_fev_line = "Hello sir! how are you!!"
+print(my_fev_line)
+my_fev_line= my_fev_line.replace("sir", "mam")
+print(my_fev_line)
