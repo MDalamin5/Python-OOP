d060558036d53249810df3f4652200e887b1a742
MDalamin5 <mdal.amin5@northsouth.edu>
2023-10-19 19:12:38 +0600

    Py

--- Python Libraries/Python-Basics/Week 01/Loop/Concept/for-loop.py
+++ Python Libraries/Python-Basics/Week 01/Loop/Concept/for-loop.py
@@ -1,59 +1,59 @@
 number = [1,2,3,4,5,6,55,65,3,6,45]
 monty_expense = [1223,4543, 34534, 3456, 34567, 6754]
 
-# total = 0
-# for x in number:
-#     total += x
-# print(total)
+total = 0
+for x in number:
+    total += x
+print(total)
 
-# total_value = 0
-# for i in range(len(number)):
-#     print(number[i])
-#     total_value += number[i]
-#     i += 1
+total_value = 0
+for i in range(len(number)):
+    print(number[i])
+    total_value += number[i]
+    i += 1
 
-# print(total_value)
-# total_cost = 0
-# for i in range(len(monty_expense)):
-#     print(f'Month: {i+1}-->Cost: {monty_expense[i]}')
-#     total_cost += monty_expense[i]
+print(total_value)
+total_cost = 0
+for i in range(len(monty_expense)):
+    print(f'Month: {i+1}-->Cost: {monty_expense[i]}')
+    total_cost += monty_expense[i]
 
-# print(f'\nYour Total cost is: {total_cost}')
+print(f'\nYour Total cost is: {total_cost}')
 
-# for i in range(len(monty_expense)):
-#     if monty_expense[i] > 4000:
-#         print(f'Month: {i+1}-->has Expence: {monty_expense[i]}')
+for i in range(len(monty_expense)):
+    if monty_expense[i] > 4000:
+        print(f'Month: {i+1}-->has Expence: {monty_expense[i]}')
 
-# print(" ")
-# for i, expence in enumerate(monty_expense):
-#     if expence > 5000:
-#         print(f"Month: {i+1} has expecnce is : {expence}")
-#         break
+print(" ")
+for i, expence in enumerate(monty_expense):
+    if expence > 5000:
+        print(f"Month: {i+1} has expecnce is : {expence}")
+        break
 
 # Finding Car Key
-# location = ['chair', 'Table', 'Wire-House', 'Book-shop', 'University' ]
+location = ['chair', 'Table', 'Wire-House', 'Book-shop', 'University' ]
 
-# got_loc = input("Enter Your Loaction: ")
-# for i , loc in enumerate(location):
-#     if got_loc == loc:
-#         print(f'Key Is Found\n')
-#         break
+got_loc = input("Enter Your Loaction: ")
+for i , loc in enumerate(location):
+    if got_loc == loc:
+        print(f'Key Is Found\n')
+        break
 
-# for i in range(1,11):
-#     if i%2 != 0:
-#         print(f"{i}-->ODD")
+for i in range(1,11):
+    if i%2 != 0:
+        print(f"{i}-->ODD")
 
-# n = 0
-# while n<=10:
-#     print(n, end=',') 
-#     n+=1
+n = 0
+while n<=10:
+    print(n, end=',') 
+    n+=1
 
-# for n in range(1,-6, -2):
-#     print(n, end=',')
+for n in range(1,-6, -2):
+    print(n, end=',')
 
-# print()
-# for num in range(0, len(number), 2):
-#     print(number[num], end=',')
+print()
+for num in range(0, len(number), 2):
+    print(number[num], end=',')
 
 x = -1
 while x <=99:
