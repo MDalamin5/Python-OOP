e5eac69503988c04a707dd4a50b615988ea937a9
MDalamin5 <mdal.amin5@northsouth.edu>
2023-10-20 09:47:55 +0600

    range in loop

---
+++ Python Libraries/Python-Basics/Week 01/Loop/Concept/for-loop2.py
@@ -0,0 +1,11 @@
+hello = [1,2,3,4,5,6,7,8]
+num = []
+for i in hello:
+    num.append(i**2)
+else:
+    print("Square Value add successfully.")
+print('The num Container Is: ',num)
+
+name = 'Al Amin'
+for i in name:
+    print(i)
---
+++ Python Libraries/Python-Basics/Week 01/Loop/Concept/range.py
@@ -0,0 +1,4 @@
+num = [1,3,5,7,8,3,5,7,85,4,3,5]
+
+for i in range(0,len(num),2):
+    print(num[i])
