17f5d557918283a018c1688b84c4e49e8d9ebf1d
MDalamin5 <mdal.amin5@northsouth.edu>
2023-10-23 17:04:38 +0600

    function in py

---
+++ Python Libraries/Python-Basics/Week 02/Function/func.py
@@ -0,0 +1,18 @@
+def find_total(expence):
+    total = 0
+    for i in range(len(expence)):
+        total += expence[i]
+        # print(total)
+    return total
+
+
+
+expence_alamin = [10, 44, 233, 45]
+expence_hello = [444, 223, 244, 23]
+total = 0
+for data in expence_alamin:
+    total += data
+
+print(total)
+tot = find_total(expence_hello)
+print(tot)
