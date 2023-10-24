a76da6de0545e97dbefe47d04596a6f24bb99d27
MDalamin5 <mdal.amin5@northsouth.edu>
2023-10-24 11:08:44 +0600

    function exercise

--- Python Libraries/Python-Basics/Week 02/Function/func.py
+++ Python Libraries/Python-Basics/Week 02/Function/func.py
@@ -1,4 +1,12 @@
 def find_total(expence):
+    """_summary_
+
+    Args:
+        expence (Integer): _description_
+
+    Returns:
+        total
+    """
     total = 0
     for i in range(len(expence)):
         total += expence[i]
@@ -14,5 +22,5 @@ for data in expence_alamin:
     total += data
 
 print(total)
-tot = find_total(expence_hello)
+tot = find_total(expence_hello) 
 print(tot)
