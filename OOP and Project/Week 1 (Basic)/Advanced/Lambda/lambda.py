4fe3a8ec6e9c5ed8305531e8106d4b7d2538f404
MDalamin5 <mdal.amin5@northsouth.edu>
2024-06-06 18:41:32 +0600

    Play with python map

---
+++ OOP and Project/Week 1 (Basic)/Advanced/Lambda/lambda.py
@@ -0,0 +1,7 @@
+getSum = lambda a, b : a + b
+
+print(getSum(4, 6))
+
+getMax = lambda x, y : x if x > y else y
+
+print(getMax(55, 55))
--- OOP and Project/Week 1 (Basic)/Advanced/MAP/map.py
+++ OOP and Project/Week 1 (Basic)/Advanced/MAP/map.py
@@ -1,9 +1,11 @@
-def getEven(num):
+def makeEven(num):
     if num % 2 == 0:
         return num
+    else:
+        return num + 1
     
 
 data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-evenNum = list(map(getEven, data))
+evenNum = list(map(makeEven, data))
 print(evenNum)
